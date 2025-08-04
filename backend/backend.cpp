317b23ec536ef6933d4e80dc86ed23fbb2e1d6d6
Rinpaz <112812497+RicardoNMFilho@users.noreply.github.com>
2025-08-04 12:01:14 -0300

    update backend to use gentexts.so dynamic library

--- backend/backend.cpp
+++ backend/backend.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 
+extern "C" {
+    #include "lib_gentexts.h"
+}
+
 using namespace std;
+
+
 
 int main() {
     cout << "Backend running!" << endl;
+
+    char** texts = generate_list_random_texts();
+
+    for (int i = 0; i < 5; ++i) {
+        std::cout << texts[i] << std::endl;
+    }
+
     return 0;
 }
